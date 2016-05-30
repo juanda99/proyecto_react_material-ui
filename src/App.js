a590bd573b43f8f2f37b9acf108b25fbbf045128
juanda99 <juandacorreo@gmail.com>
2016-05-30 23:37:59 +0200

    Añadido material-ui menu. Ojo, necesitaba el MuiTheme

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Contactar from './views/Contactar'
 import Cervezas from './views/Cervezas'
 import Template from './layout/Template'
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
   // Needed for onTouchTap
@@ -15,16 +18,18 @@ injectTapEventPlugin()
 export default class App extends Component {
   render() {
     return (
-      <Router history={browserHistory}>
-        <Route path='/' component={Template}>
-          /*observa que definimos IndexRoute, que será la ruta por defecto*/
-          /*Todas las rutas siguientes están anidadas dentro de template, es decir
-          son hijas de la anterior y desde la template cargaremos la que corresponda*/
-          <IndexRoute component={Index}/>
-          <Route path='cervezas' component={Cervezas}/>
-          <Route path='/contactar' component={Contactar}/>
-        </Route>
-      </Router>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Router history={browserHistory}>
+          <Route path='/' component={Template}>
+            /*observa que definimos IndexRoute, que será la ruta por defecto*/
+            /*Todas las rutas siguientes están anidadas dentro de template, es decir
+            son hijas de la anterior y desde la template cargaremos la que corresponda*/
+            <IndexRoute component={Index}/>
+            <Route path='cervezas' component={Cervezas}/>
+            <Route path='/contactar' component={Contactar}/>
+          </Route>
+        </Router>
+      </MuiThemeProvider>    
     )
   }
 }
--- src/layout/Template.js
+++ src/layout/Template.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import Menu from './Menu.js'
 
 export default class Template extends Component {
   render() {
     return (
       <div>
         <header><h1>Sitio web de mis cervezas</h1></header>
+        <Menu/>
         <main>{this.props.children}</main>
         <footer>Realizado por juanda</footer>
       </div>
